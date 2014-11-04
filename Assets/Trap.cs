77525d728fcdaee012c7eede8dd692d622e9f245
ChristianT <ChristianT@onlyentertainment.de>
2014-11-04 02:11:42 +0100

    jj

---
+++ Assets/Trap.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Trap : MonoBehaviour
+{
+
+    public float Damage = 0.0f;
+    public Collider[] triggered
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    void OnTriggerStay(Collider otherCollider)
+    {
+        Player_Data player_Data = otherCollider.GetComponent<Player_Data>();
+        if (player_Data != null)
+        {
+            player_Data.GetDamage(10);
+        }
+
+    }
+
+}
