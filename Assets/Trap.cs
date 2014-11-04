5b4e2fe81da371be147ecbad353263b8d8e8d841
ChristianT <ChristianT@onlyentertainment.de>
2014-11-04 03:02:43 +0100

    213

--- Assets/Trap.cs
+++ Assets/Trap.cs
@@ -1,11 +1,17 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Trap : MonoBehaviour
 {
 
-    public float Damage = 0.0f;
-    public Collider[] triggered
+    public float damage = 0.0f;
+    public float cooldown = 0.0f;
+    public float resetTimer = 0.0f;
+
+
+
+    public Dictionary<Collider, float> triggeredCollider = new Dictionary<Collider, float>();
 
 
     // Use this for initialization
@@ -17,16 +23,49 @@ public class Trap : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (resetTimer > 0.0f) resetTimer -= (1 * Time.deltaTime);
+
+    //    List<Collider> toDelete = new List<Collider>();
+    //    foreach (Collider otherCollider in triggeredCollider.Keys)
+    //    {
+    //        triggeredCollider[otherCollider] -= 1.0f * Time.deltaTime;
+    //        //if (triggeredCollider[otherCollider] <= 0) toDelete.Add(otherCollider);// triggeredCollider.Remove(otherCollider);
+    //    }
+
+        //foreach (Collider otherCollider in toDelete)
+        //{
+        //    triggeredCollider.Remove(otherCollider);
+        //}
 
     }
 
 
-    void OnTriggerStay(Collider otherCollider)
+    //void OnTriggerStay(Collider otherCollider)//
+    void OnCollisionStay(Collision otherCollision)
     {
+        Collider otherCollider = otherCollision.collider;
         Player_Data player_Data = otherCollider.GetComponent<Player_Data>();
+
         if (player_Data != null)
         {
-            player_Data.GetDamage(10);
+            if (resetTimer <= 0.0f)
+            {
+                Animation animation = GetComponent<Animation>();
+                //animation.wrapMode = WrapMode.Once;
+                animation.Play();
+                
+                if (!triggeredCollider.ContainsKey(otherCollider)) triggeredCollider.Add(otherCollider, cooldown);
+                else triggeredCollider[otherCollider] = cooldown;
+                player_Data.GetDamage(damage);
+                resetTimer = cooldown;
+            }
+
+
+        }
+
+        if (!triggeredCollider.ContainsKey(otherCollider) || triggeredCollider[otherCollider] <= 0.0f)
+        {
+
         }
 
     }
